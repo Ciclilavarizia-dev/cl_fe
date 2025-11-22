262464752e878259012b2575f11830f835bb457e
ivanoice09 <ivankovicsibsion@gmail.com>
2025-11-22 18:25:50 +0100

    aggiunta di una nuova route 'login'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CardsContainer } from './features/product/cards-container/cards-container';
 import { Detail } from './features/product/cards-container/card/detail/detail';
+import { Login } from './features/auth/login/login';
 
 export const routes: Routes = [
-    { path: '', component: CardsContainer },
-  { path: 'product/:id', component: Detail }
+  { path: '', component: CardsContainer },
+  { path: 'product/:id', component: Detail },
+  { path: 'login', component: Login}
 ];
