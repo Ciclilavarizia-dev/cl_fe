c46abfeb8ea284e52e5191952a7ee5b953e3ff3a
ivanoice09 <ivankovicsibsion@gmail.com>
2025-11-24 07:56:04 +0100

    implementazione Reset Password

--- src/app/features/auth/reset-password/reset-password.ts
+++ src/app/features/auth/reset-password/reset-password.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './reset-password.html',
   styleUrl: './reset-password.css',
 })
 export class ResetPassword {
+
+  showPassword = false;
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  
 
 }
