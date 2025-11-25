c6fd6b75224ba1beda30c44ee06da7f142acf1a1
ivanoice09 <ivankovicsibsion@gmail.com>
2025-11-26 00:45:31 +0100

    migliorament UI del login e register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,15 @@ import { Login } from './features/auth/login/login';
 import { Home } from './features/home/home';
 import { Profile } from './features/profile/profile';
 import { ResetPassword } from './features/auth/reset-password/reset-password';
+import { Register } from './features/auth/register/register';
 
 export const routes: Routes = [
   { path: '', component: CardsContainer },
   { path: 'product/:id', component: Detail },
   { path: 'login', component: Login, data: { hideNavbar: true } },
+  { path: 'reset-password', component: ResetPassword, data: { hideNavbar: true } },
+  { path: 'register', component: Register, data: {hideNavbar: true }},
   { path: 'home', component: Home },
   { path: 'profile', component: Profile },
-  { path: 'reset-password', component: ResetPassword, data: { hideNavbar: true } }
+  
 ];
--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
 
+  showPassword = false;
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  registerBackend() { }
 }
