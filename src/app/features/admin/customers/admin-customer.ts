5b3638e1db63d1cde2eb874386e582b52efde022
Ericson Manalo <ericmana98@gmail.com>
2025-12-15 17:18:50 +0100

    feat: add admin-customer component with HTML and CSS files

---
+++ src/app/features/admin/customers/admin-customer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-customer',
+  imports: [],
+  templateUrl: './admin-customer.html',
+  styleUrl: './admin-customer.css',
+})
+export class AdminCustomer {
+
+}
