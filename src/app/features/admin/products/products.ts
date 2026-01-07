615ee4c647fee865d922ffb0ddd71e569025fb54
Ivan-da-casa <ivankovicsibsion@gmail.com>
2026-01-07 01:33:59 +0100

    feat: implement product management UI with search and pagination functionality

--- src/app/features/admin/products/products.ts
+++ src/app/features/admin/products/products.ts
@@ -1,33 +1,31 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductList } from '../../../shared/models/ProductList';
 import { AdminProductHttp } from '../../../shared/services/admin/admin-product-http';
 import { CommonModule } from '@angular/common';
-import { Router, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule, RouterModule],
-  templateUrl: './products.html',
-  styleUrl: './products.css',
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  templateUrl: './admin-product.html',
+  styleUrl: './admin-product.scss',
 })
-export class Products {
-
+export class Products implements OnInit {
   products: ProductList[] = [];
 
-  totalCount = 0;
-
   page = 1;
-  pageSize = 20;
-
-  sortBy = '';
-  sortDirection: 'asc' | 'desc' = 'asc';
-
-  isLoading = true;
+  pageSize = 15;
+  totalPages = 0;
 
   activeProductId: number | null = null;
 
-  constructor(private productService: AdminProductHttp, private router: Router) { }
+  hasNext = false;
+  hasPrevious = false;
+  searchTerm: string = '';
+
+  constructor(private productService: AdminProductHttp, private router: Router) {}
 
   goToEdit(productId: number, event: MouseEvent) {
     event.stopPropagation();
@@ -36,70 +34,41 @@ export class Products {
     this.router.navigate(['/admin/products/edit', productId]);
   }
 
-  ngOnInit(): void {
-    this.loadProducts();
+  deleteProduct(productId: number) {
 
-    // close popover when clicking anywhere else
-    document.addEventListener('click', this.onDocumentClick);
   }
 
-  ngOnDestroy(): void {
-    document.removeEventListener('click', this.onDocumentClick);
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  onSearch() {
+    this.page = 1;
+    this.loadProducts();
   }
 
   loadProducts() {
-    this.isLoading = true;
-
     this.productService
-      .getProducts(this.page, this.pageSize, this.sortBy, this.sortDirection)
-      .subscribe({
-        next: res => {
-          this.products = res.items;
-          this.totalCount = res.totalCount;
-          this.isLoading = false;
-        },
-        error: err => {
-          console.error(err);
-          this.isLoading = false;
-        }
+      .getProductList(this.page, this.pageSize, this.searchTerm)
+      .subscribe((res) => {
+        this.products = res.items;
+        this.totalPages = res.totalPages;
+        this.hasNext = res.hasNext;
+        this.hasPrevious = res.hasPrevious;
       });
   }
 
-  changeSort(column: string) {
-    if (this.sortBy === column) {
-      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
-    } else {
-      this.sortBy = column;
-      this.sortDirection = 'asc';
-    }
-
-    this.loadProducts();
-  }
-
   nextPage() {
-    this.page++;
-    this.loadProducts();
-  }
-
-  prevPage() {
-    if (this.page > 1) {
-      this.page--;
+    if (this.hasNext) {
+      this.page++;
       this.loadProducts();
     }
   }
 
-  get totalPages(): number {
-    return Math.ceil(this.totalCount / this.pageSize)
+  prevPage() {
+    if (this.hasPrevious) {
+      this.page--;
+      this.loadProducts();
+    }
   }
-
-  toggleActions(productId: number, event: MouseEvent) {
-    event.stopPropagation();
-    this.activeProductId = this.activeProductId === productId ? null : productId;
-  }
-
-  @HostListener('document:click')
-  onDocumentClick() {
-    this.activeProductId = null;
-  }
-
 }
--- src/app/shared/services/admin/admin-product-http.ts
+++ src/app/shared/services/admin/admin-product-http.ts
@@ -5,29 +5,42 @@ import { AdminProductEditDto } from '../../models/AdminProductEditDto';
 import { AdminCategoryDto } from '../../models/AdminCategoryDto';
 import { AdminProductModelDto } from '../../models/AdminProductModelDto';
 import { AdminProductUpdateDto } from '../../models/AdminProductUpdateDto';
+import { ProductList } from '../../models/ProductList';
+import { Page } from '../../models/Page';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminProductHttp {
-  private readonly apiUrl = 'https://localhost:7000/api/admin/products'
+  private readonly apiUrl = 'https://localhost:7000/api/admin/products';
 
   constructor(private http: HttpClient) {}
 
-  getProducts(
-    page: number,
-    pageSize: number,
-    sortBy?: string,
-    sortDirection?: 'asc' | 'desc'
-  ) {
-    return this.http.get<any>(this.apiUrl, {
-      params:  {
-        pageNumber: page,
-        pageSize: pageSize,
-        sortBy: sortBy ?? '',
-        sortDirection: sortDirection ?? 'asc'
-      }
-    });
+  // getProducts(
+  //   page: number,
+  //   pageSize: number,
+  //   sortBy?: string,
+  //   sortDirection?: 'asc' | 'desc'
+  // ) {
+  //   return this.http.get<any>(this.apiUrl, {
+  //     params:  {
+  //       pageNumber: page,
+  //       pageSize: pageSize,
+  //       sortBy: sortBy ?? '',
+  //       sortDirection: sortDirection ?? 'asc'
+  //     }
+  //   });
+  // }
+
+  getProductList(page: number, pageSize: number, search?: string): Observable<Page<ProductList>> {
+    let url = `${this.apiUrl}/?page=${page}&pageSize=${pageSize}`;
+
+    // Se 'search' esiste e non è una stringa vuota, la aggiungiamo all'URL
+    if (search && search.trim() !== '') {
+      url += `&search=${encodeURIComponent(search)}`;
+    }
+
+    return this.http.get<Page<ProductList>>(url);
   }
 
   getProduct(id: number): Observable<AdminProductEditDto> {
@@ -39,7 +52,7 @@ export class AdminProductHttp {
   }
 
   getModels() {
-    return this.http.get<AdminProductModelDto[]>(`${this.apiUrl}/models`)
+    return this.http.get<AdminProductModelDto[]>(`${this.apiUrl}/models`);
   }
 
   // Update call
